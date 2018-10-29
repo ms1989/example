7516cbd079860dac29e68572cf2c77ce3d2d762e
Exterminas <samhammermichael@gmail.com>
2018-10-29 08:30:18 +0100

    Add project files.

---
+++ ConsoleApp2/Mailer.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mail;
+
+namespace Mailer
+{
+	public class Mailer
+	{
+
+		private SmtpClient client;
+		private MailMessage mes;
+
+		public Mailer()
+		{
+			this.mes = new MailMessage("From@gmail.com", "to@gmail.com");
+			this.client = new SmtpClient();
+			client.Port = 587;
+			client.DeliveryMethod = SmtpDeliveryMethod.Network;
+			client.UseDefaultCredentials = false;
+			client.EnableSsl = true;
+			client.Credentials = new System.Net.NetworkCredential("example@gmail.com", "password");
+			client.Host = "smtp.gmail.com";
+			mes.Subject = "Test Subject";
+
+
+
+			mes.Body = "Test Body";
+
+
+
+
+
+		}
+
+
+		public void send()
+		{
+			client.Send(mes);
+
+		}
+
+	}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mailer
+{
+	class Prog
+	{
+		static void Main(string[] args)
+		{
+
+			Mailer sender = new Mailer();
+
+			sender.send();
+		
+		
+			
+		}
+	}
+}
